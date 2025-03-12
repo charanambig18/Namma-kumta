76aec225084f7bd058ae7231c5dfacb3822e3d97
charanambig18 <ambigcharan2006@gmail.com>
2025-03-12 14:26:10 +0530

    changed

--- namma-kumta/src/Component/Namma-kumta/ShopsPage.jsx
+++ namma-kumta/src/Component/Namma-kumta/ShopsPage.jsx
@@ -1,16 +1,5 @@
 import React from "react";
 import "./ShopsPage.css";
-import nammakumta from "./image/logo-nammakumta.png";
-import bell from "./image/bell-icon.png";
-import user from "./image/user-logo.png";
-import global from "./image/global.png";
-import grocery from "./image/grocery-store.png";
-import supermarket from "./image/super-market.png";
-import dress from "./image/dress-shop.png";
-import shoe from "./image/shoe-store.png";
-import jewelry from "./image/jewelry-store.png";
-import tailor from "./image/tailor-shop.png";
-import electronics from "./image/electronic-store.png";
 
 const ShopsPage = () => {
   return (
@@ -18,13 +7,17 @@ const ShopsPage = () => {
       {/* Header */}
       <header className="header">
         <div className="logo-container">
-          <img src={nammakumta} alt="Namma Kumata Logo" className="logo" />
+          <img
+            src="./image/logo-nammakumta.png"
+            alt="Namma Kumata Logo"
+            className="logo"
+          />
           <span className="app-name">Namma Kumata</span>
         </div>
         <div className="header-icons">
-          <img src={bell} alt="Notifications" />
-          <img src={user} alt="User" />
-          <img src={global} alt="Global" />
+          <img src="./image/bell-icon.png" alt="Notifications" />
+          <img src="./image/user-logo.png" alt="User" />
+          <img src="./image/global.png" alt="Global" />
         </div>
       </header>
 
@@ -38,52 +31,92 @@ const ShopsPage = () => {
       <div className="shops-list">
         <ul>
           <li>
-            <img src={grocery} alt="Grocery Store" />
+            <img src="./image/grocery-store.png" alt="Grocery Store" />
             Grocery Store
           </li>
           <li>
-            <img src={supermarket} alt="Supermarket" />
+            <img src="./image/super-market.png" alt="Supermarket" />
             Supermarket
           </li>
           <li>
-            <img src={dress} alt="Dress Shop" />
+            <img src="./image/dress-shop.png" alt="Dress Shop" />
             Dress Shop
           </li>
           <li>
-            <img src={shoe} alt="Shoe Store" />
+            <img src="./image/shoe-store.png" alt="Shoe Store" />
             Shoe Store
           </li>
           <li>
-            <img src={jewelry} alt="Jewelry Store" />
+            <img src="./image/jewelry-store.png" alt="Jewelry Store" />
             Jewelry Store
           </li>
           <li>
-            <img src={tailor} alt="Tailor Shop" />
+            <img src="./image/tailor-shop.png" alt="Tailor Shop" />
             Tailor Shop
           </li>
           <li>
-            <img src={electronics} alt="Electronics Store" />
+            <img src="./image/electronic-store.png" alt="Electronics Store" />
             Electronics Store
           </li>
           <li>
-            <img src={electronics} alt="Electronics Store" />
-            Electronics Store
+            <img src="./image/mobile-repair-shop.png" alt="Electronics Store" />
+            Mobile Repair Shop
           </li>
           <li>
-            <img src={electronics} alt="Electronics Store" />
-            Electronics Store
+            <img src="./image/car-dealership.png" alt="Electronics Store" />
+            Car Dealership
           </li>
           <li>
-            <img src={electronics} alt="Electronics Store" />
-            Electronics Store
+            <img src="./image/auto-repair-shop.png" alt="Electronics Store" />
+            Auto Repair Shop
           </li>
           <li>
-            <img src={electronics} alt="Electronics Store" />
-            Electronics Store
+            <img src="./image/spare-parts-store.png" alt="Electronics Store" />
+            Spare Parts Store
           </li>
           <li>
-            <img src={electronics} alt="Electronics Store" />
-            Electronics Store
+            <img src="./image/pharmacy.png" alt="Electronics Store" />
+            Pharmacy
+          </li>
+          <li>
+            <img src="./image/optical-store.png" alt="Electronics Store" />
+            Optical Store
+          </li>
+          <li>
+            <img src="./image/bakery.png" alt="Electronics Store" />
+            Bakery
+          </li>
+          <li>
+            <img src="./image/sweet-shop.png" alt="Electronics Store" />
+            Sweet Shop
+          </li>
+          <li>
+            <img src="./image/dairy-shop.png" alt="Electronics Store" />
+            Dairy Shop
+          </li>
+          <li>
+            <img src="./image/meat-shop.png" alt="Electronics Store" />
+            Meat Shop
+          </li>
+          <li>
+            <img src="./image/liquor-store.png" alt="Electronics Store" />
+            Liquor Store
+          </li>
+          <li>
+            <img src="./image/bookstore.png" alt="Electronics Store" />
+            Bookstore
+          </li>
+          <li>
+            <img src="./image/toy-store.png" alt="Electronics Store" />
+            Toy Store
+          </li>
+          <li>
+            <img src="./image/aquarium-store.png" alt="Electronics Store" />
+            Aquarium Store
+          </li>
+          <li>
+            <img src="./image/florist.png" alt="Electronics Store" />
+            Florist
           </li>
         </ul>
       </div>
